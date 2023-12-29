17ccb9b5059c28ae38c86296757c0965c6df991a
mehedihasan712277 <mehedihasan712277@gmail.com>
2023-12-29 18:23:14 +0600

    get operation done

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aunadi8.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const database = client.db("productDB").collection("products");
+
+
+        app.get("/", (req, res) => {
+            res.send("hello");
+        })
+
+        app.get("/data", async (req, res) => {
+            const cursor = database.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`the ap is running on port ${port}`);
+})
