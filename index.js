5b375848b639d84c8925133a3e6c375603924597
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-01-07 14:31:03 +0600

    small change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aunadi8.mongodb.net/?retryWrites=true&w=majority`;
-
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -45,12 +42,39 @@ async function run() {
 
         app.delete("/data/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) };
             const result = await database.deleteOne(query);
             res.send(result);
         })
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        app.get("/data/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await database.findOne(query);
+            res.send(result);
+        })
+
+        app.put('/data/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            console.log(id, data);
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true }
+            const updatedData = {
+                $set: {
+                    image: data.image,
+                    name: data.name,
+                    description: data.description,
+                    price: data.price,
+                    stock: data.stock,
+                }
+            }
+            const result = await database.updateOne(filter, updatedData, options);
+            res.send(result);
+        })
+
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
     }
